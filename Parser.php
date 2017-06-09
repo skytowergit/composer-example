7a57403fb375595c28a99813a1b0155840baba41
Sergey Bystrika <serg.bystrika@gmail.com>
2017-06-09 14:25:34 +0300

    Class Parser

--- Parser.php
+++ Parser.php
@@ -1,16 +1,19 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'ParserInterface.php';
 
-/**
- * Description of Parser
- *
- * @author Sergij Sergij
- */
-class Parser {
-    //put your code here
-}
+class Parser implements ParserInterface {
+    
+   public function process(string $url,string $tag) {
+        $this->url = $url;
+        $this->tag = $tag;
+        $regex = '#['.$tag.'>][\d\s\w-\s]+[<//'.$tag.']#'; //рег.выраж. тег></тег
+        $content = file_get_contents($url); //получаем контент страницы
+        preg_match_all($regex, $content,  $matches);//делаем выборку содержимого тега
+	//var_dump($matches);
+        return $matches;
+        }
+    };
+  // $test = new Parser;
+// $test->process('https://www.w3schools.com/css/','h3');
+?>
